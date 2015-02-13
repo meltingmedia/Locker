256172f0f8181bc2f8e53cb0578d9ca85ef48941
Romain <romain@melting-media.com>
2015-02-13 15:32:48 +0100

    Lexicon

--- core/lexicon/en/default.inc.php
+++ core/lexicon/en/default.inc.php
@@ -1,12 +1,12 @@
 <?php
-
-$_lang['locker.manager_locked_message'] = '<div class="warning">We are in maintenance!</div>';
-
-$_lang['setting_locker.status_off_on_lock'] = 'Change site status on lock';
-$_lang['setting_locker.status_off_on_lock_desc'] = 'Whether or not to change the site status to "offline" when locking, and "online" when unlocking.';
+/**
+ * @var array $_lang
+ */
+$_lang['locker.manager_locked_message'] = '<div class="warning">Manager currently locked.</div>';
 
 $_lang['setting_locker.flush_sessions_on_lock'] = 'Flush sessions on lock';
 $_lang['setting_locker.flush_sessions_on_lock_desc'] = 'Whether or not to flush all users sessions when locking the manager.';
-
 $_lang['setting_locker.locked'] = 'Locked';
 $_lang['setting_locker.locked_desc'] = 'Whether or not the manager is currently locked.';
+$_lang['setting_locker.status_off_on_lock'] = 'Change site status on lock';
+$_lang['setting_locker.status_off_on_lock_desc'] = 'Whether or not to change the site status to "offline" when locking, and "online" when unlocking (site_status system setting).';
---
+++ core/lexicon/fr/default.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @var array $_lang
+ */
+$_lang['locker.manager_locked_message'] = '<div class="warning">Manager actuallement verrouillé.</div>';
+
+$_lang['setting_locker.flush_sessions_on_lock'] = 'Vider les sessions au verrouillage';
+$_lang['setting_locker.flush_sessions_on_lock_desc'] = 'Indique s\'il faut automatiquement purger les sessions utilisateur lors du verrouillage.';
+$_lang['setting_locker.locked'] = 'Verrouillé';
+$_lang['setting_locker.locked_desc'] = 'Indique si le manager est actuellement verrouillé.';
+$_lang['setting_locker.status_off_on_lock'] = 'Changer le status du site au verrouillage';
+$_lang['setting_locker.status_off_on_lock_desc'] = 'Indique s\'il faut automatique changer le status du site en "hors ligne" lors du verrouillage et "en ligne" lors du dé-verrouillage (paramètre système site_status).';
